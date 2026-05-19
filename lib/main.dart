86d6779a486d79e107f5e338f02d198badb5f0d5
billelrns <matbakhwalid@gmail.com>
2026-05-19 14:49:39 +0000

    fix: restore main.dart from clean commit to fix truncation

--- lib/main.dart
+++ lib/main.dart
@@ -5849,7 +5849,6 @@ class _CommunityPageState extends State<CommunityPage> {
                           ]),
                         ),
                         // Post text
-                        Padding(
                           padding: EdgeInsets.fromLTRB(16, 12, 16, 12),
                           child: Text(post['text'] ?? '', style: TextStyle(fontSize: 15, height: 1.6)),
                         ),
@@ -6213,4 +6212,3 @@ class _AIChatPageState extends State<AIChatPage> {
     );
   }
 }
- 
